bd0934959991e8e8213225d73131211293709c5c
LJ <juranek.lukasz@gmail.com>
2019-03-18 11:25:39 +0100

    Python lib draft add

--- python_lib/LUC.py
+++ python_lib/LUC.py
@@ -1,26 +1,82 @@
 import serial
 import math
+import time
+
+class LINFrame:
+    id = 0
+    data = b'0'
+
 
 class LUC:
     def __init__(self, com):
         self.ser = serial.Serial(com,timeout=1) 
 
+    def flushData(self, data):
+        self.ser.write(data)
+        print(data)
+    
+
     def reqestFirmwareVersion(self):
-        self.ser.write(b'v\r')
+        self.flushData(b'v\r')
         line = self.ser.readline().decode("utf-8")         
         return line.replace("v","").replace("\r","")
 
     def openAsMaster(self):
-        self.ser.write(b'O\r')        
+        self.flushData(b'O\r')        
+        return (self.ser.readline().decode("utf-8") == '\r')
+
+    def openAsSlave(self):
+        self.flushData(b'L\r')        
+        return (self.ser.readline().decode("utf-8") == '\r')
+
+    def openAsMonitor(self):
+        self.flushData(b'l\r')        
+        return (self.ser.readline().decode("utf-8") == '\r')
+
+    def close(self):
+        self.flushData(b'C\r')        
         return (self.ser.readline().decode("utf-8") == '\r')
 
     def addTransmitFrameToTable(self,id, data):
         data_str = hex(data).replace("0x","")
         data_len = math.floor(len(data_str)/2)
-        line = ('t'+ hex(id).replace("0x","") + str(data_len) + data_str + '\r') 
-        self.ser.write(line.encode())
-        return (self.ser.readline().decode("utf-8") == '\r')
+        line = ('t0' + hex(id).replace("0x","").rjust(2,'0') + str(data_len) + data_str + '\r') 
+        self.flushData(line.encode())
+        return (self.ser.readline().decode("utf-8") == 'z\r')
 
+    def addReceptionFrameToTable(self,id, len):
+        line = ('r0' + hex(id).replace("0x","").rjust(2,'0') + str(len) + '\r') 
+        self.flushData(line.encode())
+        return (self.ser.readline().decode("utf-8") == 'z\r')
 
+    def enable(self):
+        self.flushData(b'r1ff0\r')
+        return (self.ser.readline().decode("utf-8") == 'z\r')
+
+    def disable(self):
+        self.flushData(b'r2ff0\r')
+        return (self.ser.readline().decode("utf-8") == 'z\r')
+
+    def waitForFrame(self, wait_time_ms):
+        count = 0
+        sleep_time = 0.002
+        frame = ''
+        while (True):   
+            while (self.ser.in_waiting>0): #if incoming bytes are waiting to be read from the serial input buffer
+                char = self.ser.read(1).decode('ascii')
+                frame += char
+                if (char == '\r'):
+                    print(frame)
+                    lf = LINFrame()                
+                    lf.id = int(frame[2:4],16)
+                    lf.data = int(frame[5:-1],16)
+                    return lf                         
+            time.sleep(sleep_time)
+            count += 1
+            if (count > wait_time_ms/sleep_time/1000):
+                return 0 
+
+        
     def __del__(self):
+        self.close()
         self.ser.close()
--- python_lib/LUC_test.py
+++ python_lib/LUC_test.py
@@ -3,18 +3,42 @@ import LUC
 
 class LUCTesting(unittest.TestCase):
     def setUp(self):
-        self.luc = LUC.LUC('COM7')
+        self.master = LUC.LUC('COM30')
+        self.slave = LUC.LUC('COM7')
+
+        ll = self.master.close()
+        ll = self.slave.close()
+
     def tearDown(self):
-        del self.luc
+        del self.master
+        del self.slave
+
     def test_firmware_version(self):        
-        ll = self.luc.reqestFirmwareVersion()
+        ll = self.master.reqestFirmwareVersion()
         self.assertEqual(ll, '0102')
-    def test_master(self):
-        ll = self.luc.openAsMaster()        
-        ll = self.luc.addTransmitFrameToTable(0x1,0x2233)        
+        ll = self.slave.reqestFirmwareVersion()
+        self.assertEqual(ll, '0102')
+
+    def test_comunication(self):
+        ll = self.master.openAsMaster() 
+        ll = self.slave.openAsSlave()       
+        self.assertEqual(ll, 1)
+        
+        ll = self.master.disable()
+        ll = self.slave.disable()
+        self.assertEqual(ll, 1)        
+        
+        ll = self.master.addTransmitFrameToTable(0x1,0x2233)
+        ll = self.slave.addReceptionFrameToTable(0x1,0x2)
+        self.assertEqual(ll, 1)                
+        
+        ll = self.slave.enable()
+        ll = self.master.enable()        
         self.assertEqual(ll, 1)
 
-
+        ll = self.slave.waitForFrame(2000)
+        self.assertEqual(ll.id, 1)
+        self.assertEqual(ll.data, 0x2233)
 
 def main():
     unittest.main()
